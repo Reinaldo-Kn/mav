1c9cedd39b40e9f29e3560a2cba8944ab3b004f5
Reinaldo-Kn <reinaldokneto@gmail.com>
2022-01-05 20:45:13 -0300

    0.02

---
+++ Exercício 2/ex2.lua
@@ -0,0 +1,24 @@
+function removeEven(array)
+    for i , elem in ipairs(array) do
+         if elem % 2 == 0 then
+            table.remove (array, i)
+            
+         end
+         
+    end
+end
+function callAll(array) 
+for j = #array, 1,-1 do
+removeEven(array)
+end
+end
+
+
+array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+callAll(array) 
+print(table.concat(array, ' '))
+
+
+array = { 1, 3, 4, 6, 7, 8, 10 }
+callAll(array) 
+print(table.concat(array, ' ')) 
---
+++ Exercíco 1/ex1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+void comparacao(int valor){
+    if(((valor % 3) == 0) && ((valor % 5 ) == 0)){
+        printf("fizzbuzz \n");
+    }
+    else if((valor % 3) == 0){
+        printf("fizz \n");
+    }
+    else if((valor % 5) == 0){
+        printf("buzz \n");
+    }
+    else
+    printf("%d \n",valor);
+}
+
+
+int main()
+{
+        int num = 1;
+        for(num; num <= 100; num++){
+            comparacao(num);
+        }
+    return 0;    
+}
